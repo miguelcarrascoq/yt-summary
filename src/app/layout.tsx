1eb87c4b1eb60838884640a849d43c591c0442b0
Miguel Carrasco Q <miguel@visitanyplace.com>
2024-07-17 11:44:54 -0400

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { AntdRegistry } from '@ant-design/nextjs-registry';
 import { App, ConfigProvider } from 'antd';
 import theme from './media/themeConfig';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata: Metadata = {
   title: "📺 Youtube summary",
@@ -24,6 +25,7 @@ export default function RootLayout({
             </App>
           </AntdRegistry>
         </body>
+        <GoogleAnalytics gaId="G-6CTJ9JXML1" />
       </html>
     </ConfigProvider>
   );

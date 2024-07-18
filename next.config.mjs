f360bd279ac93fedc7cffefb77aa924fe6a430ad
Miguel Carrasco Q <miguel@visitanyplace.com>
2024-07-17 23:19:16 -0400

    fix: adding remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['i1.ytimg.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**.ytimg.com',
+            }
+        ]
     },
 };
 

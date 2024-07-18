013d7588309e706175b98f8356ac7838c3e09cf8
Miguel Carrasco Q <miguel@visitanyplace.com>
2024-07-17 23:19:33 -0400

    clean

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,17 @@
 // Import the NextResponse type from 'next/server'
 import { NextRequest, NextResponse } from 'next/server';
 
-// This is the middleware function
 export function middleware(request: NextRequest) {
-  // Retrieve the 'X-Yt-Summary' header from the request
   const headerYTSummary = request.headers.get('X-Yt-Summary');
 
-  // Compare the header value with the environment variable
   if (headerYTSummary !== process.env.NEXT_PUBLIC_CRYPTO_SECRET) {
-    // If they don't match, return a JSON response indicating an invalid request
     return new Response(
       JSON.stringify({
         status: false,
         message: 'Invalid request 🙃',
       }),
       {
-        status: 403, // Forbidden status code
+        status: 403,
         headers: {
           'Content-Type': 'application/json',
         },
@@ -23,7 +19,6 @@ export function middleware(request: NextRequest) {
     );
   }
 
-  // If the header matches, continue with the request
   return NextResponse.next();
 }
 

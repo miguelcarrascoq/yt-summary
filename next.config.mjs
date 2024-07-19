800a98ee56754bd050ddccd541a5215d12e6cca1
Miguel Carrasco Q <miguel@visitanyplace.com>
2024-07-18 23:01:32 -0400

    remove cspHeader

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`
 const nextConfig = {
     images: {
         remotePatterns: [
@@ -19,19 +6,6 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: '**.ytimg.com',
             }
-        ]
-    },
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'Content-Security-Policy',
-                        value: cspHeader.replace(/\n/g, '') + " script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com; script-src-elem 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com;",
-                    },
-                ],
-            },
         ]
     },
 };

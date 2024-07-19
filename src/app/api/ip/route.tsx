ea92337ee231d12b6b44503a1eae3a5eae65487f
Miguel Carrasco Q <miguel@visitanyplace.com>
2024-07-18 22:34:53 -0400

    remove nonce

--- src/app/api/ip/route.tsx
+++ src/app/api/ip/route.tsx
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(req: NextRequest) {
-    const nonce = req.headers.get("x-nonce")
     const ip = (req.headers.get('X-Forwarded-For') ?? '127.0.0.1').split(',')[0]
-    return NextResponse.json({ ip, nonce })
+    return NextResponse.json({ ip })
 }

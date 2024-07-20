accbbaf345deb4409e18b5cd32e767c1e6894cb9
Miguel Carrasco Q <miguel@visitanyplace.com>
2024-07-20 10:05:13 -0400

    add cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,39 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+const allowedOrigins = ['https://yt-summary-next.vercel.app'];
+
+const corsOptions = {
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+};
+
 export function middleware(request: NextRequest) {
+  // Check the origin from the request
+  const origin = request.headers.get('origin') ?? '';
+  const isAllowedOrigin = allowedOrigins.includes(origin);
+
+  // Handle preflighted requests
+  const isPreflight = request.method === 'OPTIONS';
+
+  if (isPreflight) {
+    const preflightHeaders = {
+      ...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
+      ...corsOptions,
+    };
+    return NextResponse.json({}, { headers: preflightHeaders });
+  }
+
+  // Handle simple requests
+  const response = NextResponse.next();
+
+  if (isAllowedOrigin) {
+    response.headers.set('Access-Control-Allow-Origin', origin);
+  }
+
+  Object.entries(corsOptions).forEach(([key, value]) => {
+    response.headers.set(key, value);
+  });
+
   const headerYTSummary = request.headers.get('X-Yt-Summary');
 
   if (headerYTSummary !== process.env.NEXT_PUBLIC_CRYPTO_SECRET) {
@@ -18,7 +51,7 @@ export function middleware(request: NextRequest) {
     );
   }
 
-  return NextResponse.next();
+  return response;
 }
 
 export const config = {

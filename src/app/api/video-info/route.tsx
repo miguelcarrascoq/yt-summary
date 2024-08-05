b4372db957c6de23276ce0df8eaaf0536208ba90
Miguel Carrasco Q <miguel@visitanyplace.com>
2024-08-04 22:00:47 -0400

    try getting comments (ideas)

--- src/app/api/video-info/route.tsx
+++ src/app/api/video-info/route.tsx
@@ -3,6 +3,8 @@ import { IVideoDataResponse, IYoutubeDataApiResponse } from './interface';
 import { CONST_USE_USER_API_KEY, CONST_YOUTUBE_API_KEY } from '@/app/services/constants';
 import { headers } from 'next/headers'
 
+// const maxResults = 100;
+
 export async function GET(request: NextRequest): Promise<NextResponse<IVideoDataResponse>> {
     const headersList = headers();
     const userApiKey = headersList.get("X-User-Api-Key");
@@ -18,15 +20,18 @@ export async function GET(request: NextRequest): Promise<NextResponse<IVideoData
 
         const apiKey = CONST_USE_USER_API_KEY ? userApiKey : CONST_YOUTUBE_API_KEY;
         const resTitle: Response = await fetch(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&part=snippet,contentDetails,statistics&key=${apiKey}`)
+        // const resComments: Response = await fetch(`https://www.googleapis.com/youtube/v3/commentThreads?key=${apiKey}&textFormat=plainText&part=snippet&videoId=${videoId}&maxResults=${maxResults}`)
 
         const titleResponse: IYoutubeDataApiResponse = await resTitle.json();
+        // const commentsResponse: IYoutubeDataApiResponse = await resComments.json();
         return NextResponse.json({
             status: true,
             data: {
                 videoId: videoId,
                 title: titleResponse.items[0].snippet.title,
                 thumbnail: `https://i1.ytimg.com/vi/${videoId}/hqdefault.jpg`,
-                extra: titleResponse
+                extra: titleResponse,
+                // comments: commentsResponse
             }
         });
     } catch (error) {
